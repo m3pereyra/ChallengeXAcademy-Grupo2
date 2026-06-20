24268163a686307d53d26911c5202064e3311b9c
braiancarrizo24 <braiancarrizo245@gmail.com>
2026-06-20 17:16:10 -0300

    Hice una actualizacion de mis casos de prueba

--- cypress/e2e/rooms/rooms.cy.js
+++ cypress/e2e/rooms/rooms.cy.js
@@ -10,18 +10,21 @@ describe ('Rooms',() =>{
   it ('Verificacion del boton "Today" en single room', () => {
      cy.navigate()
      cy.get(':nth-child(1) > .card > .card-footer > .btn').click()
+     cy.get('.rbc-toolbar')
      cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(1)').should('be.visible').and('contain', 'Today').click()
   })
   
   it('Verificacion del boton "Back" en single room', () => {
     cy.navigate()
     cy.get(':nth-child(1) > .card > .card-footer > .btn').click()
+    cy.get('.rbc-toolbar')
     cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(2)').should('be.visible').and('contain', 'Back').click()  
  })
 
   it('Verificacion del boton "Next" en single room',()=>{
     cy.navigate()
     cy.get(':nth-child(1) > .card > .card-footer > .btn').click()
+    cy.get('.rbc-toolbar')
     cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(3)').should('be.visible').and('contain', 'Next').click()
   })
 
