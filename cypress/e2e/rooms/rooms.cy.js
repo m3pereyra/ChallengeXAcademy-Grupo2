bae8e74a842b5d31d5f890bdf59f9263506e3684
braiancarrizo24 <braiancarrizo245@gmail.com>
2026-06-20 17:07:32 -0300

    Realice mi segundo caso de prueba de verificacion de botones, este y el anterior fueron positivos

--- cypress/e2e/rooms/rooms.cy.js
+++ cypress/e2e/rooms/rooms.cy.js
@@ -11,6 +11,12 @@ describe ('Rooms',() =>{
      cy.navigate()
      cy.get(':nth-child(1) > .card > .card-footer > .btn').click()
      cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(1)').should('be.visible').and('contain', 'Today').click()
+  })
+  
+  it('Verificacion del boton "Back" en single room', () => {
+    cy.navigate()
+    cy.get(':nth-child(1) > .card > .card-footer > .btn').click()
+    cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(2)').should('be.visible').and('contain', 'Back').click()  
 
 
     
