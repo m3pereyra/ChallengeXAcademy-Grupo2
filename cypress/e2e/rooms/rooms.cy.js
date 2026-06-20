59bfbd6a42e21f3bcd0033d06e6c43522d675343
braiancarrizo24 <braiancarrizo245@gmail.com>
2026-06-20 20:28:22 -0300

    Estoy probando matchear elementos de un react big calendar donde ya instale cypress-real-events

--- cypress/e2e/rooms/rooms.cy.js
+++ cypress/e2e/rooms/rooms.cy.js
@@ -9,24 +9,56 @@ describe ('Rooms',() =>{
 
   it ('Verificacion del boton "Today" en single room', () => {
      cy.navigate()
-     cy.get(':nth-child(1) > .card > .card-footer > .btn').click()
      cy.get('.rbc-toolbar')
      cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(1)').should('be.visible').and('contain', 'Today').click()
   })
   
   it('Verificacion del boton "Back" en single room', () => {
     cy.navigate()
-    cy.get(':nth-child(1) > .card > .card-footer > .btn').click()
     cy.get('.rbc-toolbar')
     cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(2)').should('be.visible').and('contain', 'Back').click()  
  })
 
-  it('Verificacion del boton "Next" en single room',()=>{
+  it('Verificacion del boton "Next" en single room',() => {
     cy.navigate()
-    cy.get(':nth-child(1) > .card > .card-footer > .btn').click()
     cy.get('.rbc-toolbar')
     cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(3)').should('be.visible').and('contain', 'Next').click()
   })
+  
+  it('Verificacion de reserva fallida de single rooms',()=>{
+    cy.navigate()
+    cy.get('.rbc-toolbar')
+    cy.get('.rbc-toolbar > :nth-child(1) > :nth-child(2)').should('be.visible').and('contain', 'Back').click()
+    cy.get('.rbc-calendar')
+    cy.get('.rbc-day-bg')
+    cy.contains('.rbc-button-link', '01')
+    .trigger('mousedown', { button: 0, force: true })
+    cy.get('.rbc-day-bg')
+    cy.contains('.rbc-button-link', '07')
+    .trigger('mousemove', { force: true })
+    .trigger('mouseup', { button: 0, force: true })
+    
+    
+  })
+
+    it('Verificacion de reserva fallida de single rooms', () => {
+
+  cy.navigate()
+
+  cy.contains('button', 'Back')
+    .click()
+
+  cy.contains('.rbc-button-link', '01')
+    .realMouseDown()
+
+  cy.contains('.rbc-button-link', '07')
+    .realHover()
+    .realMouseUp()
+
+})
+   
+  
+
 
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,14 +3,12 @@ Cypress.Commands.add('accederPaginaAdmin', () => {
 })
 
 Cypress.Commands.add('navigate', () => {
-  cy.get('.nav-link')
-    .contains('Rooms')
-    .click()
+  cy.get(':nth-child(1) > .nav-link').click()
 
-  cy.get('.card-title')
-    .contains('Single')
+  cy.get('.row > :nth-child(1) > .card > .card-body > .card-title')
     .should('be.visible')
+    .and('contain', 'Single')
 
-  cy.contains('button, a', 'Book Now')
+  cy.get(':nth-child(1) > .card > .card-footer > .btn')
     .click()
 })
