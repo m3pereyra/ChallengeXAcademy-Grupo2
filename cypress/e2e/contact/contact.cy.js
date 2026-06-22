e44b5e53ed91714e1a2f74b7188de6b2b73bc292
Julian Chaves <julianichaves@hotmail.com>
2026-06-22 20:19:43 -0300

    corregi identacion

--- cypress/e2e/contact/contact.cy.js
+++ cypress/e2e/contact/contact.cy.js
@@ -14,8 +14,6 @@ describe('Formulario de Contacto', () => {
     it('Debería enviar el formulario de contacto con datos válidos y mostrar confirmación', () => {
         // Cargo los datos desde el fixture
         cy.fixture('contact.json').then((data) => {
-
-
             cy.get('[data-testid="ContactName"]').type(data.nombre);
             cy.get('[data-testid="ContactEmail"]').type(data.email);
             cy.get('[data-testid="ContactPhone"]').type(data.telefono);
@@ -24,16 +22,15 @@ describe('Formulario de Contacto', () => {
 
             // Intercepto la petición de API 
             cy.intercept('POST', '**/message*').as('mensaje');
-
-
             cy.get('.d-grid > .btn').click();
 
             // Validar la respuesta de la API (tendria que ser 201 pero la pag me devuelve un 200)
             cy.wait('@mensaje').its('response.statusCode').should('eq', 200);
 
             // Valido la confirmación del formulario
-            cy.get('.col-lg-8 > .card > .card-body > .h4').should('contain.text', `Thanks for getting in touch ${data.nombre}`);
+            cy.get('.col-lg-8 > .card > .card-body > .h4').should('contain.text', `Thanks for getting in touch ${data.nombre}`
 
+            );
         });
     });
 });
